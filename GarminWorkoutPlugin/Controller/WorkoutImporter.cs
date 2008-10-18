e127b6ee617e7ba0d8982bb9b872023451bfd8b8
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2008-10-18 12:57:55 +0000

    - Fixed bug where we were getting rid of end of lines in notes

--- GarminWorkoutPlugin/Controller/WorkoutImporter.cs
+++ GarminWorkoutPlugin/Controller/WorkoutImporter.cs
@@ -22,11 +22,16 @@ namespace GarminWorkoutPlugin.Controller
                 importStream.Read(byteContents, 0, (int)importStream.Length);
                 stringContents = Encoding.UTF8.GetString(byteContents, 0, (int)importStream.Length);
 
-                // Remove all non-printing characters
+                // Akward bug fix : Remove last character if it's a non-printing character
                 for (int i = 0; i < 32; ++i)
                 {
                     char currentCharacter = (char)i;
-                    stringContents = stringContents.Replace(currentCharacter.ToString(), "");
+
+                    if (stringContents.EndsWith(currentCharacter.ToString()))
+                    {
+                        stringContents = stringContents.Substring(0, stringContents.Length - 1);
+                        break;
+                    }
                 }
 
                 document.LoadXml(stringContents);
