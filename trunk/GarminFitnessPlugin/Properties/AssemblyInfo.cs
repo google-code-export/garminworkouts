d8bf0218016f6ce91755bad2580230ec79366a1f
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2008-12-06 15:40:28 +0000

    - Wrong version number, now 164

--- trunk/GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ trunk/GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.161")]
+[assembly: AssemblyVersion("1.0.164")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
