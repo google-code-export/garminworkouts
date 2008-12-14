fb3e2ff60676349f66ebe9a9008c000f9327f4bf
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2008-12-14 14:39:49 +0000

    - Updated norwegian strings

--- GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.180")]
+[assembly: AssemblyVersion("1.0.181")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
