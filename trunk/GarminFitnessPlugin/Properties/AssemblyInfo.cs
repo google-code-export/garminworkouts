fe49eec52b72aefe2067b748dfeb87f23b157230
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2009-01-07 02:05:25 +0000

    - Updated localized text

--- trunk/GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ trunk/GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.209")]
+[assembly: AssemblyVersion("1.0.210")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
