7bf016a4ce870daf8e3191d7a214b582f7f98751
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2010-07-21 11:01:49 +0000

    - Updated reference dlls in depot

--- branches/ST3/GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ branches/ST3/GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.267")]
+[assembly: AssemblyVersion("1.1.268")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
