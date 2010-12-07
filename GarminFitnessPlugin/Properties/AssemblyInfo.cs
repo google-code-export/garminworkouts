ecc2260794ec7e289aa069effbe9780c655e1b1f
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2010-12-07 12:49:02 +0000

    - Updated cn-TW translation

--- GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.376")]
+[assembly: AssemblyVersion("1.1.377")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
