ac9f83de581ba9f6511610c6e3337d8d5570da32
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2011-01-19 01:24:19 +0000

    - Updated Framework to fix non admin user crash on Vista & Win7

--- GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.393")]
+[assembly: AssemblyVersion("1.1.394")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
