780d5c0ac08971892f603a70931b63b85d994826
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2011-03-11 12:21:52 +0000

    - Fixed link step parent workout modification not trigering a refresh of containing workout

--- GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.424")]
+[assembly: AssemblyVersion("1.1.425")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
