9b0c6f8a57ab21d1708f46d2c989a1052814efd0
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2011-12-29 18:05:32 +0000

    - Remove "Export from file" option

--- trunk/GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ trunk/GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.452")]
+[assembly: AssemblyVersion("1.1.453")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
