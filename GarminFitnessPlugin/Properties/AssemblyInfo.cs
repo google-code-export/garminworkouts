143308ddc6c17393a7a59cab901bc89a0631879b
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2012-01-04 11:31:15 +0000

    - Updated version #

--- GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.453")]
+[assembly: AssemblyVersion("1.1.455")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
