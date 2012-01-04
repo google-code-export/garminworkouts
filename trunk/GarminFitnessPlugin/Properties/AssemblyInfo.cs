ddf5a3cd8eef163690bd7eadb2c65b69d3fbdf76
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2012-01-04 16:34:04 +0000

    - Removed forcing FIT sport to cycling since FR610XT & 310XT seem to support FIT files lately.

--- trunk/GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ trunk/GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.455")]
+[assembly: AssemblyVersion("1.1.456")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
