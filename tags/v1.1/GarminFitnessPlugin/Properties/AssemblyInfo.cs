cd3a8870f05ff2553104c16bd43cdebbb8b5dcf6
cabest@bluebottle.com <cabest@bluebottle.com@111c52d6-838d-11dd-99d6-6900107db72b>
2012-11-24 14:12:33 +0000

    - Fixed FIT repeat deserialization bug

--- tags/v1.1/GarminFitnessPlugin/Properties/AssemblyInfo.cs
+++ tags/v1.1/GarminFitnessPlugin/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.490")]
+[assembly: AssemblyVersion("1.1.498")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
